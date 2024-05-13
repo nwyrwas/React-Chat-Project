4f80ddf718310aa224f1927b76ecd977e60263d3
Nick Wyrwas <nick.wyrwas@outlook.com>
2024-05-13 16:24:19 -0500

    <Changes to app.js>

--- src/App.js
+++ src/App.js
@@ -38,17 +38,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h1>React Bot </h1>
+          <p>Created by nick wyrwas</p>
         <div>
           {chat.map((message, index) => (
             <p key={index}><strong>{message.user}:</strong> {message.text}</p>
